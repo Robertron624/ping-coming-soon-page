80d92ac4b41dd71c417b34d47a84b3b0ab017152
Roberto Ramirez <robert.624@hotmail.com>
2023-04-05 23:28:42 -0500

    mobile design finished, email validation added

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <script lang="ts">
-  export default {
-    data() {
-      return {
-        msg: 'Welcome to Your Vue.js + TypeScript App',
-        email: '',
-      }
+export default {
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js + TypeScript App',
+      email: '',
+      emailError: '',
     }
-  }
+  },
+  methods: {
+    validateEmail(email: string) {
+      const re = /\S+@\S+\.\S+/;
+      return re.test(email);
+    },
+    submitForm() {
+      if (this.validateEmail(this.email)) {
+        this.emailError = '';
+      } else {
+        this.emailError = 'Please provide a valid email address';
+      }
+      console.log("submitting form")
+    },
+  },
+}
 
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="/logo.svg"/>
+    <img alt="Vue logo" class="logo" src="/logo.svg" />
   </header>
 
   <main>
     <div class="upper">
       <h1>We are launching <strong>soon!</strong></h1>
       <p> Subscribe and get notified </p>
-      <form action="POST">
+      <form @submit.prevent="submitForm" action="POST">
         <label for="email"></label>
-        <input placeholder="Your email address..." type="text" id="email">
+        <div class="input-container">
+          <input :class="{ 'border-error': emailError, 'border-normal': !emailError}" v-model="email" placeholder="Your email address..." type="text" id="email">
+          <span v-if="emailError" class="input-error">{{ emailError }}</span>
+        </div>
         <button type="submit">Notify Me</button>
       </form>
     </div>
@@ -29,6 +47,16 @@
       <img class="hero" src="/illustration-dashboard.png" alt="hero">
     </div>
   </main>
+  <footer>
+    <div class="social-networks">
+      <ul>
+        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+      </ul>
+    </div>
+    <span class="copyright">&copy; Copyright Ping. All rights reserved.</span>
+  </footer>
 </template>
 
 <style scoped lang="scss">
@@ -57,23 +85,26 @@ header {
     width: 80px;
     height: 25px;
   }
+
   margin-bottom: 2rem;
 }
 
 main {
   padding: 0 1rem;
+  margin-bottom: 5rem;
 }
 
-.hero-container{
+.hero-container {
   display: flex;
   justify-content: center;
+
   .hero {
     width: 90%;
     margin-inline: auto;
   }
 }
 
-.upper{
+.upper {
 
   text-align: center;
   margin-bottom: 3rem;
@@ -86,12 +117,21 @@ main {
     flex-direction: column;
     gap: .9rem;
 
-    button,input {
+    button{
       width: 90%;
       height: 2.6rem;
       border-radius: 20px;
-      border: 1px solid $pale-blue;
+      border: none;
     }
+    .input-container {
+      width: 100%;
+      input{
+        width: 90%;
+        height: 2.6rem;
+        border-radius: 20px;
+      }
+    }
+
     input {
       padding-left: 1rem;
 
@@ -99,6 +139,22 @@ main {
         color: $gray;
         padding-left: 1rem;
       }
+    }
+
+    .border-normal {
+      border: 1px solid $pale-blue;
+    }
+
+    .border-error {
+      border: 1px solid $light-red;
+    }
+
+    .input-error {
+      color: $light-red;
+      font-size: .8rem;
+      font-weight: 400;
+      text-align: center;
+      font-style: italic;
     }
 
     button {
@@ -126,6 +182,45 @@ main {
     margin-bottom: 1rem;
   }
 
+}
+
+footer {
+
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 2rem;
+
+  .social-networks {
+    margin-bottom: 2rem;
+    ul {
+      display: flex;
+      list-style: none;
+      padding: 0;
+      gap: 2rem;
+  
+      li {
+  
+        border: 1px solid $pale-blue;
+        width: 2rem;
+        height: 2rem;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+  
+        i {
+          color: $blue;
+        }
+      }
+    }
+  }
+
+  span.copyright {
+    color: $gray;
+    font-size: .8rem;
+  }
 }
 
 </style>
